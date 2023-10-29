80f02bc10c130f997e29122526a53d69f75fbb22
anasteyshha6 <glazunova.anastassia@yandex.ru>
2023-10-29 23:04:15 +0300

    завтра

---
+++ assets/js/math.js
@@ -0,0 +1,15 @@
+function summation(a, b) {
+    return a + b;
+}
+
+function subtraction(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function division(a, b) {
+    return a / b;
+}
